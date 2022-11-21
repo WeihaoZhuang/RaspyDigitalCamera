2b3bacd2fb45be50cc42af497b3b39b0db2224ac
WeihaoZhuang <zhuangweihao1996@gmail.com>
2022-11-21 06:09:42 +0000

    update

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import cv2
 import re
 import rawpy
 import pilgram
+import torch
 import numpy as np
 import picamera as picam
+from sid_model import preprocessing, init_sid_model
 from PIL import Image
 from pathlib import Path
 from glob import glob
@@ -13,9 +15,42 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QThreadPool, pyqtSlot, pyqtSignal
 from pidng.core import RPICAM2DNG
 
+from picamera import mmal, mmalobj, exc
+from picamera.mmalobj import to_rational
+
+
+MMAL_PARAMETER_ANALOG_GAIN = mmal.MMAL_PARAMETER_GROUP_CAMERA + 0x59
+MMAL_PARAMETER_DIGITAL_GAIN = mmal.MMAL_PARAMETER_GROUP_CAMERA + 0x5A
 
 us2s = 1000000
-img_folder = "/home/pi/workspace/digtial_camera/images/"
+img_folder = "./images/"
+def set_gain(camera, gain, value):
+    """Set the analog gain of a PiCamera.
+    
+    camera: the picamera.PiCamera() instance you are configuring
+    gain: either MMAL_PARAMETER_ANALOG_GAIN or MMAL_PARAMETER_DIGITAL_GAIN
+    value: a numeric value that can be converted to a rational number.
+    """
+    if gain not in [MMAL_PARAMETER_ANALOG_GAIN, MMAL_PARAMETER_DIGITAL_GAIN]:
+        raise ValueError("The gain parameter was not valid")
+    ret = mmal.mmal_port_parameter_set_rational(camera._camera.control._port, 
+                                                    gain,
+                                                    to_rational(value))
+    if ret == 4:
+        raise exc.PiCameraMMALError(ret, "Are you running the latest version of the userland libraries? Gain setting was introduced in late 2017.")
+    elif ret != 0:
+        raise exc.PiCameraMMALError(ret)
+
+def set_analog_gain(camera, value):
+    """Set the gain of a PiCamera object to a given value."""
+    set_gain(camera, MMAL_PARAMETER_ANALOG_GAIN, value)
+
+def set_digital_gain(camera, value):
+    """Set the digital gain of a PiCamera object to a given value."""
+    set_gain(camera, MMAL_PARAMETER_DIGITAL_GAIN, value)
+    
+    
+    
 
 class ClickableImageLabelListener():
     def __init__(self, ):
@@ -76,8 +111,6 @@ class ClickableImageLabel(QtWidgets.QLabel):
         pixmap = QtGui.QPixmap.fromImage(self.img)
         main_label.setPixmap(pixmap)
 
-
-
 class CameraMenu(object):
     def __init__(self,):
         self.widget = QtWidgets.QWidget()
@@ -100,7 +133,8 @@ class CameraMenu(object):
         self.filter_text_label = []
         self.current_label = [None]
         
-
+        self.sid_model = init_sid_model("/home/pi/workspace/digtial_camera/pth.txt")
+        
     def set_main_viewer(self, main_label):
         for label in self.image_label:
             label.set_main_viewer(main_label)
@@ -153,15 +187,19 @@ class CameraMenu(object):
         self.delete_act = QtWidgets.QAction("Delete", self.widget)
         self.vis_dng_act = QtWidgets.QAction("Visual DNG", self.widget)
         self.vis_filters_act = QtWidgets.QAction("Filters", self.widget)
+        self.super_night_act = QtWidgets.QAction("Super Night", self.widget)
+        
         
         self.delete_act.triggered.connect(self.option_menu_motion)
         self.vis_dng_act.triggered.connect(self.option_menu_motion)
         self.vis_filters_act.triggered.connect(self.option_menu_motion)
+        self.super_night_act.triggered.connect(self.option_menu_motion)
+        
         
         self.option_menu.addAction(self.delete_act)
         self.option_menu.addAction(self.vis_dng_act)
         self.option_menu.addAction(self.vis_filters_act)
-
+        self.option_menu.addAction(self.super_night_act)
         
         self.toolButton.setMenu(self.option_menu)
  
@@ -180,6 +218,26 @@ class CameraMenu(object):
             self.widget_filter.show()
             self.visual_filters()
             
+        if self.widget.sender() == self.super_night_act:
+            self.super_night()
+            
+    @torch.no_grad()
+    def super_night(self, ):
+        file_name = self.toolButton.text()
+        file_stem = file_name.split(".")[0]
+        file_type = file_name.split(".")[1]
+        file_path = f"{img_folder}/{file_name}"
+        
+        if file_type == 'dng':
+            inp = preprocessing(file_path)
+            out = self.sid_model(torch.tensor(inp[:, :, :2016])[None])[0]
+            out = torch.clip(out, 0, 1)
+            out = out.permute(1,2,0).numpy()[:,:,::-1] * 255.
+            out = out.astype("uint8")
+            cv2.imwrite(f"{img_folder}/{file_stem}_SN.jpeg", out)
+            self.load_images(init=True)  
+            
+            
     def visual_dng(self,):
         file_name = self.toolButton.text()
         file_stem = file_name.split(".")[0]
@@ -188,7 +246,7 @@ class CameraMenu(object):
         
         if file_type == "dng":
             raw = rawpy.imread(file_path).postprocess(use_camera_wb=True, half_size=True)[:,:,::-1]
-            cv2.imwrite(f"{img_folder}/{file_stem}.jpeg", raw)
+            cv2.imwrite(f"{img_folder}/{file_stem}_VIS.jpeg", raw)
             self.load_images(init=True)            
     
     def visual_filters(self, ):
@@ -200,12 +258,12 @@ class CameraMenu(object):
             file_path = f"{img_folder}/{file_name}"
         
             img = cv2.imread(file_path)
-            img = cv2.resize(img,(301,181))
+            img = cv2.resize(img, (320, 240))
             img = Image.fromarray(img)
             img = filter_func(img)
             img = np.ascontiguousarray(np.array(img)[:,:,::-1])
      
-            img = QtGui.QImage(img, 301, 181, 3*301, QtGui.QImage.Format_RGB888)
+            img = QtGui.QImage(img, 320, 240, 3*320, QtGui.QImage.Format_RGB888)
             img_resized = img.scaled(75, 75, aspectRatioMode=QtCore.Qt.KeepAspectRatio, 
                                   transformMode = QtCore.Qt.FastTransformation)
             
@@ -252,7 +310,7 @@ class CameraMenu(object):
         for i, file in enumerate(img_paths, st):
 
             img = QtGui.QImage(file)
-            img_large = img.scaled(320,int(320/4*3),aspectRatioMode=QtCore.Qt.KeepAspectRatio, transformMode = QtCore.Qt.FastTransformation)
+            img_large = img.scaled(320, 240,aspectRatioMode=QtCore.Qt.KeepAspectRatio, transformMode = QtCore.Qt.FastTransformation)
             img_resized = img.scaled(75,75,aspectRatioMode=QtCore.Qt.KeepAspectRatio, transformMode = QtCore.Qt.FastTransformation)
 
             image_label = ClickableImageLabel(QtGui.QPixmap.fromImage(img_resized), Path(file).name, Path(file).name)
@@ -287,12 +345,22 @@ class CameraMenu(object):
         cv2.imwrite(f"{img_folder}/{file_stem}.JPG", img)
         self.load_images(init=True)
     
+    def init_sid_model(self,):
+        pth = open("./pth.txt", 'rb')
+        pth.seek(0)
+        model = SeeInDark()
+        model = model.eval()
+        model = quantize_fx.prepare_fx(model, {'': torch.quantization.default_qconfig})
+        model = quantize_fx.convert_fx(model)
+        model.load_state_dict(torch.load(pth))
+        self.sid_model = model
        
     def init_ui(self,):
         self.setupUi()
         self.retranslateUi()
         self.load_images(True)
         self.set_option_menu()
+        
         
     def setupUi(self,):
         Form = self.widget
@@ -321,14 +389,14 @@ class CameraMenu(object):
         self.toolButton.setPopupMode(QtWidgets.QToolButton.MenuButtonPopup)
         self.toolButton.setObjectName("toolButton")
         self.MainImagelabel = QtWidgets.QLabel(Form)
-        self.MainImagelabel.setGeometry(QtCore.QRect(5, 5, 320, int(320/4*3)))
+        self.MainImagelabel.setGeometry(QtCore.QRect(5, 5, 320, 240))
         self.MainImagelabel.setText("")
         self.MainImagelabel.setObjectName("MainImagelabel")
         
         
         
         
-        self.widget_filter.setGeometry(QtCore.QRect(10, 210, 461, 101))
+        self.widget_filter.setGeometry(QtCore.QRect(10, 250, 580, 101))
         self.widget_filter.setObjectName("widget")
         self.widget_filter.hide()
 
@@ -336,10 +404,10 @@ class CameraMenu(object):
         self.scrollArea_2.setWidgetResizable(True)
         self.scrollArea_2.setObjectName("scrollArea_2")
         self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(10, 210, 461, 101))
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(10, 250, 580, 101))
         self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
         self.gridLayoutWidget_2 = QtWidgets.QWidget(self.scrollAreaWidgetContents_2)
-        self.gridLayoutWidget_2.setGeometry(QtCore.QRect(10, 210, 461, 101))
+        self.gridLayoutWidget_2.setGeometry(QtCore.QRect(10, 250, 580, 101))
         self.gridLayoutWidget_2.setObjectName("gridLayoutWidget_2")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.scrollAreaWidgetContents_2)
         self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
@@ -377,14 +445,11 @@ class CameraMenu(object):
 
 
 
-
-
-
-
 class PreviewWindow(object):
     def __init__(self,):
         self.window = QtWidgets.QMainWindow()
         self.camera = picam.PiCamera()
+        set_digital_gain(self.camera, 1)
         self.iso_step = 10
         self.shutter_step = [10,13,15,20,25,30,40,50,60,80,100,125,160,200,250,320,400,500,640,800,1000,1250,1600,2000,2500,3200,4000]
         self.dng_convert = RPICAM2DNG()
@@ -447,11 +512,9 @@ class PreviewWindow(object):
         if len(os.listdir(img_folder)) == 0:
             img_idx = 0
         else:
-            img_idx = max([int(x.split(".")[0]) for x in os.listdir(img_folder)]) + 1
+            img_idx = max([int(x.split(".")[0].split("_")[0]) for x in os.listdir(img_folder)]) + 1
         return img_idx   
-    
-#     def show_menu(self, menu):
-#         menu.widget.show()
+
     
     def show_main_window(self,):
         self.window.show() 
@@ -592,8 +655,6 @@ class CameraApp(object):
     
         
         sys.exit(self.app.exec_())
-
-
 
 app = CameraApp()
 
